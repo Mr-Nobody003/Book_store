9e585b7642d4696ff6e76387fb0a223b79fbf71b
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2024-10-17 18:14:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:  process.env.GITHUB_PAGES === 'true'
+  ? '/Book_store_frontend/' // Base path for GitHub Pages
+  : undefined, // Root path for Vercel
 })
